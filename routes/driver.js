5e9269174037c50ff59264d21f251134f72fd463
harry123001 <yello@Yellos-MacBook-Air.local>
2023-11-02 12:49:33 -0400

    Changes

--- routes/driver.js
+++ routes/driver.js
@@ -5,10 +5,10 @@ import * as controllers from "../controllers/drivers.js";
 const router = Router();
 
 router.get("/", controllers.getDrivers);
-router.get("/driver/id/:driverId", controllers.getDriverById);
+router.get("/id/:driverId", controllers.getDriverById);
 router.post("/", controllers.createDriver);
 router.put("/:id", controllers.updateDriver);
 router.delete("/:driverId", controllers.deleteDriverById);
-router.get("/driver/firstname/:firstName", controllers.getDriverByFirstName);
+router.get("/firstname/:firstName", controllers.getDriverByFirstName);
 
 export default router;
